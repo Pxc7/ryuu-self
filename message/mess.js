e0e0daae90716cbddf6435b659c99e76a7db9d8e
Pxc7 <pxc7bp0@gmail.com>
2021-10-20 23:06:30 +0700

    ryuu-bot

---
+++ message/mess.js
@@ -0,0 +1,20 @@
+module.exports.mess = {
+	        wait: 'Loading...',
+	       limit: `[❕] Limit kamu sudah habis silahkan kirim !limit untuk mengecek limit`,
+			success: 'Oke Desu~',
+			wrongFormat: 'Format salah, coba liat lagi di menu',
+			error: {
+				api: 'Ups, terjadi kesalahan',
+				stick: 'salah sticker',
+				Iv: 'Link tidak valid'
+			},
+			only: {
+				group: 'Maaf! Command ini khusus untuk di dalam Group saja.',
+				admin: 'Maaf! Command ini khusus untuk admin group saja.',
+				prem: `*_Kamu belum Terdaftar di Database Premium!!_\n_Ketik !owner untuk Sewa Premium!_*`,
+				
+				premium: 'Kamu bukan user premium, kirim perintah *!buypremium* untuk membeli premium',
+				owner: 'Maaf! Command ini khusus untuk Owner Bot saja.',
+				Badmin: 'Maaf! Command ini khusus untuk Bot ketika jadi admin!!',
+			}
+		}
